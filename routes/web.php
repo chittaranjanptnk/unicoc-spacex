2059222b049d3adb324849f72c15c73ee2d666ab
chittap <chittaranjan.pattnaik@mindfiresolutions.com>
2022-02-05 07:30:08 +0530

    Added the keys in .env.example and removed unused routes

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LaunchController;
-use App\Http\Controllers\GoogleDiveController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,6 +17,3 @@ use App\Http\Controllers\GoogleDiveController;
 Route::get('/', [LaunchController::class, 'index'])->name('home-launches');
 Route::get('/launches', [LaunchController::class, 'index'])->name('launches');
 Route::get('/launches/{flightNumber}', [LaunchController::class, 'show'])->name('launches-show')->where('flightNumber', '[0-9]+');
-
-Route::get('/google-drive/set-token/{code?}', [GoogleDiveController::class, 'setToken'])->name('google-drive-set-token');
-Route::get('/google-drive/upload-token', [GoogleDiveController::class, 'uploadToken'])->name('google-drive-upload-token');
